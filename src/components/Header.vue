e69807eccb88ab1ba3493274cb5b934c3c380ce6
SimoneSpirito <simospiri01@gmail.com>
2021-12-13 14:32:16 +0100

    header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,24 @@
 <template>
   <header>
-
+      <div class="container">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="logo">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Movie</a></li>
+                    <li><a class="active" href="#">Movie</a></li>
+                    <li><a href="#">Movie</a></li>
+                    <li><a href="#">Movie</a></li>
+                    <li><a href="#">Movie</a></li>
+                    <li><a href="#">Movie</a></li>
+                    <li><a href="#">Movie</a></li>
+                    <li><a href="#">Movie</a></li>
+                    <li><a href="#">Movie</a></li>
+                    <li><a href="#">Movie</a></li>
+                </ul>
+            </nav>
+      </div>
   </header>
 </template>
 
@@ -10,6 +28,39 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import url('../assets/style/partials/variables.scss');
+
+.container {
+    height: 120px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    img {
+        width: 70px;
+    }
+
+    ul {
+        font-size: 14px;
+        text-transform: uppercase;
+        list-style: none;
+        display: flex;
+
+        a {
+            display: block;
+            height: 120px;
+            line-height: 120px;
+            color: #1c1c1c;
+            text-decoration: none;
+            margin: 0 10px;
+
+            &.active, &:hover {
+                color: #2982f9;
+                border-bottom: 4px solid #2982f9;
+            }
+        }
+    }
+}
 
 </style>
